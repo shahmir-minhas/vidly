a8f333d58b2e23d1a23203bfc50eda59d8302752
Shahmir Minhas <62132242+shahmir-minhas@users.noreply.github.com>
2021-11-26 15:10:16 +0500

    vidly-fixes

---
+++ src/containments/containment.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const Parent = () => {
+    return ( 
+        <React.Fragment>
+            
+        </React.Fragment>
+     );
+}
+ 
+export default Parent;
